156afac335a57e92287882e4680174f13d1836be
Jakub Šuráň <suranjakub@email.cz>
2025-11-02 15:55:09 +0100

    Fix margin style for subtitle

--- src/components/subtitle/subtitle.tsx
+++ src/components/subtitle/subtitle.tsx
@@ -1,5 +1,5 @@
 import { subtitleProps } from "./types";
 
 export default function Subtitle(props: subtitleProps) {
-  return <h2 className="mb-8 text-center text-3xl">{props.text}</h2>;
+  return <h2 className="my-8 text-center text-3xl">{props.text}</h2>;
 }
