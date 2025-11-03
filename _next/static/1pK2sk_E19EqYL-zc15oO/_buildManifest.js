f34e6320e765c47aad0f261e8ebbeef16d7da44d
Jakub Šuráň <suranjakub@email.cz>
2025-11-04 00:25:14 +0100

    Updates

--- _next/static/1pK2sk_E19EqYL-zc15oO/_buildManifest.js
+++ _next/static/1pK2sk_E19EqYL-zc15oO/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/taxMachine//_next/:path+",
+        "destination": "/taxMachine/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
